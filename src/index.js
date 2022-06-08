e330707fd9feae4aaa63014b92f523521feeae18
MaksymBezrodnyiTR <Maksym.Bezrodnyi@thomsonreuters.com>
2022-06-08 15:18:02 +0300

    added init position + refactor

--- src/index.js
+++ src/index.js
@@ -1,28 +1,58 @@
-import { argsConstructor } from "./utils/argsParsing.js";
-import { templateGreeting, defaultGreetingKey, defaultTmpl, defaultName } from "./constants.js";
 import { stdin, stdout } from "node:process";
 import { Transform, Duplex } from "stream";
-import { Stream } from "stream";
+import path from "path";
+import { fileURLToPath } from "url";
+import os from "os";
+import { argsConstructor } from "./utils/argsParsing.js";
+import { insertDataInsteadTmpl } from "./utils/insertDataInsteadTmpl.js";
+import {
+  templateGreeting,
+  defaultGreetingKey,
+  defaultTmpl,
+  defaultName,
+  templateBye,
+  currentPositionMessage,
+} from "./constants.js";
+
+const currentPosition = os.homedir();
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const args = process.argv.slice(2);
 const argsObj = argsConstructor(args);
 
 const userName = argsObj[defaultGreetingKey] || defaultName;
-const greetingString = templateGreeting.replace(defaultTmpl, userName);
+const greetingString = insertDataInsteadTmpl(templateGreeting, defaultTmpl, userName);
+const buyString = insertDataInsteadTmpl(templateBye, defaultTmpl, userName);
 
 const stream = new Duplex({
   read(r) {
     // console.log(r);
   },
-  // transform(chunk, encoding, cb) {
-  //   this.push(chunk);
-  //   cb();
-  // },
+  transform(chunk, encoding, cb) {
+    this.push(chunk);
+    cb();
+  },
   write(chunk, encoding, cb) {
     this.push(chunk);
     cb();
   },
 });
-stream.push(greetingString);
+
+stream
+  .on("close", () => console.log("close"))
+  .on("error", () => console.log("error"))
+  .on("finish", () => console.log("finish"))
+  .on("end", () => console.log("end"));
+
+process.on("SIGINT", function () {
+  console.log(buyString);
+  process.exit();
+});
+
+//Initial greeting and init position
+stream.push(greetingString + "\n");
+stream.push(`${currentPositionMessage} ${currentPosition}\n`);
 
 stdin.pipe(stream).pipe(stdout);
---
+++ src/utils/insertDataInsteadTmpl.js
@@ -0,0 +1,3 @@
+export const insertDataInsteadTmpl = (stringWithTmpl, defaultTmpl, data) => {
+  return stringWithTmpl.replace(defaultTmpl, data);
+};
